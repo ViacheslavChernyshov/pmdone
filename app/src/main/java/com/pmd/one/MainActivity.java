c8d6db66efa627cbfaf963dafb7e4e0f782d24a8
AledFeen <vovabond849@gmail.com>
2022-05-11 16:35:55 +0300

    Seventh

--- app/src/main/java/com/pmd/one/MainActivity.java
+++ app/src/main/java/com/pmd/one/MainActivity.java
@@ -3,12 +3,37 @@ package com.pmd.one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void increaseQuantity(View view)
+    {
+        if(quantity >= 10){
+            quantity = 10;
+        } else quantity = quantity + 1;
+
+
+        TextView CountText = findViewById(R.id.CountTextView);
+        CountText.setText("" + quantity);
+    }
+
+    public void decreaseQuantity(View view)
+    {
+        if (quantity <= 0) {
+            quantity = 0;
+        } else quantity = quantity - 1;
+        TextView CountText = findViewById(R.id.CountTextView);
+        CountText.setText("" + quantity);
+
     }
 }
