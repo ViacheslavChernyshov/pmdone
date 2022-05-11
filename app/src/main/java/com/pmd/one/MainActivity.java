45eda40172da533e1bc19d7a752a618c61c37150
AledFeen <vovabond849@gmail.com>
2022-05-11 18:12:43 +0300

    Eighth

--- app/src/main/java/com/pmd/one/MainActivity.java
+++ app/src/main/java/com/pmd/one/MainActivity.java
@@ -4,16 +4,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
 
-    int quantity = 0;
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Integer quantity = 0;
+    Spinner spinner;
+    ArrayList spinnerArrayList;
+    ArrayAdapter spinnerAdapter;
+
+    HashMap goodsMap;
+    String goodsName;
+    double price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createSpinner();
+        createMap();
+
+    }
+
+    public void createMap()
+    {
+        goodsMap = new HashMap();
+        goodsMap.put("guitar", 500.0);
+        goodsMap.put("drums", 1500.0);
+        goodsMap.put("keyboard", 1000.0);
+    }
+
+    public void createSpinner()
+    {
+        spinner = findViewById(R.id.Spinner);
+        spinner.setOnItemSelectedListener(this);
+        spinnerArrayList = new ArrayList();
+
+        spinnerArrayList.add("guitar");
+        spinnerArrayList.add("drums");
+        spinnerArrayList.add("keyboard");
+
+        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
     }
 
     public void increaseQuantity(View view)
@@ -21,10 +62,10 @@ public class MainActivity extends AppCompatActivity {
         if(quantity >= 10){
             quantity = 10;
         } else quantity = quantity + 1;
-
-
         TextView CountText = findViewById(R.id.CountTextView);
         CountText.setText("" + quantity);
+        TextView priceTextView = findViewById(R.id.PriceTextView);
+        priceTextView.setText("" + quantity * price);
     }
 
     public void decreaseQuantity(View view)
@@ -34,6 +75,30 @@ public class MainActivity extends AppCompatActivity {
         } else quantity = quantity - 1;
         TextView CountText = findViewById(R.id.CountTextView);
         CountText.setText("" + quantity);
+        TextView priceTextView = findViewById(R.id.PriceTextView);
+        priceTextView.setText("" + quantity * price);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        goodsName = spinner.getSelectedItem().toString();
+        price = (double)goodsMap.get(goodsName);
+        TextView priceTextView = findViewById(R.id.PriceTextView);
+        priceTextView.setText("" + quantity * price);
+
+        ImageView goodImageView = findViewById(R.id.goodImageView);
+
+        if(goodsName.equals("guitar")) {
+            goodImageView.setImageResource(R.drawable.git);
+        } else if(goodsName.equals("drums")) {
+            goodImageView.setImageResource(R.drawable.drums);
+        }else if(goodsName.equals("keyboard")) {
+            goodImageView.setImageResource(R.drawable.piano);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
