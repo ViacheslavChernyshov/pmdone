df057cdb191785004e17e657480b2440211b6200
Арт Дим <deadbox33@gmail.com>
2022-05-12 14:12:06 +0300

    Market 2

--- app/src/main/java/com/pmd/one/MainActivity.java
+++ app/src/main/java/com/pmd/one/MainActivity.java
@@ -3,12 +3,29 @@ package com.pmd.one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
+    TextView quantityTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void increaseQuantity(View view) {
+        quantity++;
+        quantityTextView = findViewById(R.id.quantityTextView);
+        quantityTextView.setText("" + quantity);
+    }
+
+    public void decreaseQuantity(View view) {
+        quantity--;
+        quantity = (quantity < 0) ? (quantity = 0) : quantity;
+        quantityTextView = findViewById(R.id.quantityTextView);
+        quantityTextView.setText("" + quantity);
     }
 }
