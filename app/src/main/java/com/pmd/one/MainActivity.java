a52ed6d723357f653cb45a3a778ea5d0eac049f3
enderdez100 <enderdez100@gmail.com>
2022-05-19 12:27:04 +0300

    Video

--- app/src/main/java/com/pmd/one/MainActivity.java
+++ app/src/main/java/com/pmd/one/MainActivity.java
@@ -5,30 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean checked = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void goAnimation(View view) {
-        ImageView humanOneImageView = findViewById(R.id.OneImageView);
-        ImageView humanTwoImageView = findViewById(R.id.TwoImageView);
-        if (checked) {
-            humanOneImageView.animate().alpha(0).rotation(humanOneImageView.getRotation() - 1080)
-                    .scaleX(0).scaleY(0).setDuration(2000);
-            humanTwoImageView.animate().alpha(1).rotation(humanTwoImageView.getRotation() -1080)
-                    .scaleX(1).scaleY(1).setDuration(2000);
-            checked = false;
-        } else {
-            humanOneImageView.animate().alpha(1).rotation(humanOneImageView.getRotation() - 1080)
-                    .scaleX(1).scaleY(1).setDuration(2000);
-            humanTwoImageView.animate().alpha(0).rotation(humanTwoImageView.getRotation() - 1080)
-                    .scaleX(0).scaleY(0).setDuration(2000);
-            checked = true;
-        }
+        VideoView videoView = findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() +
+                "/" + R.raw.video);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
     }
 }
