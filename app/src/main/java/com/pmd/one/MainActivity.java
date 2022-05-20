85ccf93fa5cf3f85ab450acaf529a4a7c20b75bc
enderdez100 <enderdez100@gmail.com>
2022-05-20 12:02:30 +0300

    Music

--- app/src/main/java/com/pmd/one/MainActivity.java
+++ app/src/main/java/com/pmd/one/MainActivity.java
@@ -2,24 +2,58 @@ package com.pmd.one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.MediaController;
-import android.widget.VideoView;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
+    Button playPauseButton;
+    SeekBar volumeSeekBar;
+    AudioManager audioManager;
+    boolean checked = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        VideoView videoView = findViewById(R.id.videoView);
-        videoView.setVideoPath("android.resource://" + getPackageName() +
-                "/" + R.raw.video);
-        MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView(videoView);
-        videoView.setMediaController(mediaController);
-        videoView.start();
+
+        audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        volumeSeekBar = findViewById(R.id.volumeSeekBar);
+        volumeSeekBar.setMax(maxVolume);
+        volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.kalimba);
+    }
+
+    public void playPause(View view) {
+        playPauseButton = findViewById(R.id.playPauseButton);
+        if (checked) {
+            mediaPlayer.start();
+            playPauseButton.setText("Pause");
+            checked = false;
+        } else {
+            mediaPlayer.pause();
+            playPauseButton.setText("Play");
+            checked = true;
+        }
+
     }
 }
